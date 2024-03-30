e65b41d834241dac77f56bbcd1fd05c7843838fb
aaron <aaron.lhh0401@gmail.com>
2024-03-30 01:21:23 -0700

    update

--- components/List.jsx
+++ components/List.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const List = ({handleButton, updateDate, list}) => {
+const List = ({handleButton, updateDate, updatePhase, list}) => {
     return (
         <div className="List">
             <div className="filters">
@@ -11,32 +11,45 @@ const List = ({handleButton, updateDate, list}) => {
                         onChange={(e) => updateDate(e.target.value)}
                     />
                 </div>
-                <button class="btn" onClick={handleButton}>Search</button>
+                <div className="phaseFilter">
+                    <label for="moonphase">Moon Phase: </label>
+                    <input 
+                        type="range" 
+                        id="moonphase"
+                        name="moonphase"
+                        min='0.0'
+                        max='1.0'
+                        step='0.1'
+                        onChange={(e) => updatePhase(e.target.value)}
+                    />
+                </div>
+                <button className="btn" onClick={handleButton}>Search</button>
             </div>
             
             <div className="table">
-                <thead>
-                    <tr>
-                        <th>Date</th>
-                        <th>Temperature</th>
-                        <th>Time</th>
-                        <th>Phase</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        list && 
-                        Object.entries(list.data).map(([index]) => {
-                            <tr>
-                                <td>{list.data[index].valid_date}</td>
-                                <td>{list.data[index].temp}</td>
-                                <td>{new Date(list.data[index].moonrise_ts*1000).toUTCString().slice(-11, -4)}</td>
-                                <td>{list.data[index].moon_phase}</td>
-                            </tr>
-                        })
-                        
-                    }
-                </tbody>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Temperature</th>
+                            <th>Time</th>
+                            <th>Phase</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            list && 
+                            Object.entries(list).map(([index]) => 
+                                <tr key={index}>
+                                    <td>{list[index].valid_date}</td>
+                                    <td>{list[index].temp}</td>
+                                    <td>{new Date(list[index].moonrise_ts*1000).toUTCString().slice(-11, -4)}</td>
+                                    <td>{list[index].moon_phase.toFixed(1)}</td>
+                                </tr>
+                            )
+                        }
+                    </tbody>
+                </table>
             </div>
         </div>
     )
--- src/App.jsx
+++ src/App.jsx
@@ -8,41 +8,70 @@ function App() {
     // date, average temparature, MoonRise time, moonphase
     const [list, setList] = useState(null);
     const [date, setDate] = useState('');
+    const [phase, setPhase] = useState('');
+    const [filterList, setFilterList] = useState(null);
     useEffect(() => {
         const fetchForcast = async () => {
             let query = `https://api.weatherbit.io/v2.0/forecast/daily?city=New+York&key=${API_KEY}`
             const response = await fetch(query);
             const json = await response.json();
             setList(json);
-            console.log(json.data['0']);
+            setFilterList(json.data);
+            console.log(json.data);
         };
         fetchForcast().catch(console.error);
     }, []);
 
     const handleButton = () => {
-        
+        let FilteredData = Object.values(list.data);
+        if (date !== ''){
+            FilteredData = FilteredData.filter((value) => 
+                value.valid_date.includes(date)
+            );
+            console.log(FilteredData);
+        }
+
+        if (phase !== ''){
+            FilteredData = FilteredData.filter((value) => 
+                value.moon_phase.toFixed(1) <= phase
+            );
+            console.log(FilteredData);
+        }
+
+        setFilterList(FilteredData);
     };
 
     return (
         <>
-            <Card 
-                number = "New York"
-                label = "New York, USA"
-            />
-            <Card 
-                number = {new Date(list.data['0']['moonrise_ts']*1000).toUTCString().slice(-11, -4)}
-                label = "Moon Rise"
-            />
-            <Card 
-                number = {`${list.data['0']['moon_phase']*100} %`}
-                label = "Moon Phase"
-            />
+            <div className='App-page'>
+                {   
+                    list && 
+                    <>
+                        <Card 
+                            number = "New York"
+                            label = "New York, USA"
+                        />
+                        <Card 
+                            number = {new Date(list.data['0']['moonrise_ts']*1000).toUTCString().slice(-11, -4)}
+                            label = "Moon Rise"
+                        />
+                        <Card 
+                            number = {`${list.data['0']['moon_phase'].toFixed(1)}`}
+                            label = "Moon Phase"
+                        />
+                    </>
+                }
 
-            <List 
-                handleButton={handleButton}
-                updateDate = {setDate}
-                list = {list}
-            />
+                {   
+                    filterList &&
+                    <List 
+                        handleButton={handleButton}
+                        updateDate = {setDate}
+                        updatePhase = {setPhase}
+                        list = {filterList}
+                    />
+                }
+            </div>
 
         </>
     )
